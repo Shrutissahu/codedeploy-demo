00b2b7a163ecaae7208dc214ff2fc7b12c57a3e0
Shruti Arora <sarora@amazon.com>
2023-04-11 16:51:09 -0400

    Update message output

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@ int main(int argc, char* argv[])
     int n = atoi(argv[1]);
     auto factorial = Factorial::GetFactorial(n);
 
-    std::cout << "Here is the Factorial for the number " << n << " = " << factorial << std::endl;    
+    std::cout << "The Factorial for the number " << n << " = " << factorial << std::endl;    
     return 0;
 }
+
+
